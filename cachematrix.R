fdf810d7e22ebc85689361ed349a94230d32a9bb
hersonpc <hersonpc@gmail.com>
2016-11-03 19:37:19 -0200

    Programming Assignment 2: Lexical Scoping

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,55 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Author: Herson Melo
+# E-mail: hersonpc@gmail.com
 
-## Write a short comment describing this function
+###############################################################################
+#
+# These functions calculates the inverse of a matrix, keeping the cached result
+# avoiding a new computational cost, if it is requested in succession.
+#
+# https://en.wikipedia.org/wiki/Invertible_matrix
+#
+###############################################################################
+
+
+
+# The "makeCacheMatrix" create a new object with the ability to save a variable
+# result in cache and returns it. 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    s <- NULL
+    set <- function(y) {
+        x <<- y
+        s <<- NULL
+    }
+    get <- function() x
+    setsolve <- function(solve) s <<- solve
+    getsolve <- function() s
+    list(set = set, get = get,
+         setsolve = setsolve,
+         getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+# The "cacheSolve" function consumes a this previous object and then solve the
+# equation, but before solving the equation, it checks for a cache to decide if
+# will return the cached value instead of processing again.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    s <- x$getsolve()
+    if(!is.null(s)) {
+        message("getting cached data")
+        return(s)
+    }
+    data <- x$get()
+    s <- solve(data, ...)
+    x$setsolve(s)
+    ## Return a matrix that is the inverse of 'x'
+    return(s)
 }
+
+
+#Tests
+cm <- makeCacheMatrix()
+cm$set(matrix(c(3,2,1,1), ncol=2, byrow = T))
+cacheSolve(cm)
+cacheSolve(cm)
